f4b0357e2cf544c597acfef862037c95122e451f
zcubbs <zackcubby@gmail.com>
2023-08-21 01:51:09 +0200

    :white_check_mark: fix tests

--- api/main_test.go
+++ api/main_test.go
@@ -1,16 +1,11 @@
 package api
 
 import (
-	"context"
-	"fmt"
-	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 	db "github.com/zcubbs/tlz/db/sqlc"
 	"github.com/zcubbs/tlz/internal/util"
 	"github.com/zcubbs/tlz/pkg/random"
-	"github.com/zcubbs/tlz/pkg/token"
 	"github.com/zcubbs/tlz/worker"
-	"google.golang.org/grpc/metadata"
 	"testing"
 	"time"
 )
@@ -30,16 +25,16 @@ func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDist
 	return server
 }
 
-func newContextWithBearerToken(t *testing.T, tokenMaker token.Maker, username string, userId uuid.UUID, duration time.Duration) context.Context {
-	accessToken, _, err := tokenMaker.CreateToken(username, userId, duration)
-	require.NoError(t, err)
-
-	bearerToken := fmt.Sprintf("%s %s", authorizationBearer, accessToken)
-	md := metadata.MD{
-		authorizationHeader: []string{
-			bearerToken,
-		},
-	}
-
-	return metadata.NewIncomingContext(context.Background(), md)
-}
+//func newContextWithBearerToken(t *testing.T, tokenMaker token.Maker, username string, userId uuid.UUID, duration time.Duration) context.Context {
+//	accessToken, _, err := tokenMaker.CreateToken(username, userId, duration)
+//	require.NoError(t, err)
+//
+//	bearerToken := fmt.Sprintf("%s %s", authorizationBearer, accessToken)
+//	md := metadata.MD{
+//		authorizationHeader: []string{
+//			bearerToken,
+//		},
+//	}
+//
+//	return metadata.NewIncomingContext(context.Background(), md)
+//}
